fc444611903ded289b0aa5ea451a48713f94ee78
Wei Minn <mg.weiminn@gmail.com>
2019-02-16 16:41:40 +0800

    BalanceBracket Check Algorithm

---
+++ Session6/BalancedBrackets.py
@@ -0,0 +1,16 @@
+from pythonds import Stack
+
+b = '[({])}'
+
+def checkBalance(string):
+    s = Stack()
+    for c in string:
+        if c in b:
+            if b.index(c) < 3:
+                s.push(c)
+            else:
+                if s.pop() != b[b.index(c)-3]:
+                    return False
+    return True
+
+print(checkBalance('[ a + { b / ( c - d ) + e / (f + g ) } - h ]'))
