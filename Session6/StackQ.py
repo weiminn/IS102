b32c9b6fea4c4d3b966ab1513d4ba4030fb05808
Wei Minn <mg.weiminn@gmail.com>
2019-02-16 17:40:18 +0800

    simulated queue using stack

---
+++ Session6/StackQ.py
@@ -0,0 +1,29 @@
+from pythonds import Stack
+
+class StackQ:
+    s1 = Stack()
+    s2 = Stack()
+
+    def __init__(self):
+        print('Stack Q initiated')
+
+    def nQ(self, x):
+        self.s1.push(x)
+
+    def dQ(self):
+        while not self.s1.isEmpty():
+            self.s2.push(self.s1.pop())
+
+        toReturn = self.s2.pop()
+        
+        while not self.s2.isEmpty():
+            self.s1.push(self.s2.pop())
+
+        return toReturn
+
+sq = StackQ()
+sq.nQ(1)
+sq.nQ(2)
+sq.nQ(3)
+
+print(sq.dQ(), sq.dQ(), sq.dQ())
